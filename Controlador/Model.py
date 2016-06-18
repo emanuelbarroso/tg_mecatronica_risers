44e54c7a80e8b0a9828d91f4ff32783b58963b30
Emanuel B <bm2983@gmail.com>
2016-06-18 15:30:15 -0300

    add a Model test

--- Controlador/Model.py
+++ Controlador/Model.py
@@ -3,7 +3,7 @@ import numpy
 from ReducedModel import ReducedModel
 from DelayBlock import DelayBlock
 from Kalman import Kalman
-from Controller import Controller
+#from Controller import Controller
 
 class Model:
 	def __init__(self,n,A,B,C,D,Ak,Bk,Ck,Q,R,epsilon,Ts):
@@ -14,3 +14,23 @@ class Model:
 
 	def closed_loop(self,y_top,y_bottom):
 		return y_ref
+
+if __name__ == "__main__":
+	n = 4
+	A = numpy.random.rand(4,4)
+	B = numpy.random.rand(4,1)
+	C = numpy.random.rand(1,4)
+	D = numpy.random.rand(1,1)
+	Ak = numpy.random.rand(4,4)
+	Bk = numpy.random.rand(4,1)
+	Ck = numpy.random.rand(1,4)
+	Q = 0.01**2 * numpy.eye(4)
+	R = 0.1**2
+	epsilon = 0.3
+	Ts = 0.1
+	model = Model(n,A,B,C,D,Ak,Bk,Ck,Q,R,epsilon,Ts)
+	print(model.delay_block.dim)
+	lst = []
+	for i in range(0,6):
+		lst.append(model.reduced_model.compute_y(1))
+	print(lst)
